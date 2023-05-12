4f832ef4b366c9b31f0fdd4612e348b5f84e2200
TijanaTicjka <tijanamilenkovic13@gmail.com>
2023-05-13 00:19:24 +0200

    Updated badge style on detailed card

--- src/components/DetailedCharacter/DetailedCharacter.js
+++ src/components/DetailedCharacter/DetailedCharacter.js
@@ -17,12 +17,22 @@ export const DetailedCharacter = () => {
 
     if (!character) return null;
 
+     let badgeClass = "badge-detailed";
+
+    if (character.status === "Alive") {
+        badgeClass += " bg-success";
+    } else if (character.status === "Dead") {
+        badgeClass += " bg-danger";
+    } else {
+        badgeClass += " bg-secondary";
+    }
+
     return(
         <Container fluid>
             <Row className='character-detailed d-flex justify-content-center align-items-center'>
                 <Card style={{ width: '24rem' }} className="text-center card-detailed  gap-2 align-items-center mt-5">
                     <Card.Header className='card-header-detailed bg-info text-white border-bottom-5'>{character.name}</Card.Header>
-                    <Badge className="badge-detailed">{character.status}</Badge>
+                    <Badge className={badgeClass}>{character.status}</Badge>
                     <Card.Img variant="top" src={character.image} className="rounded border border-white"/>
                     <ListGroup variant="flush" className="rounded list">
                         <ListGroup.Item className="fw-bold"><span className="text-secondary fw-normal">Gender:</span> {character.gender}</ListGroup.Item>
