30629fc8b8ebec379ea3431992b4b70ae5aa5d81
TijanaTicjka <tijanamilenkovic13@gmail.com>
2023-05-12 23:54:06 +0200

    fixed problem for deploying

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -35,7 +35,7 @@ export const Home = () => {
                 setCharacters([]);
               };
         });
-    }, [timeForCall, searchTerm]);
+    }, [timeForCall, searchTerm, pageNumber, pageForCall]);
 
     return(
         <Container fluid className='home'>
